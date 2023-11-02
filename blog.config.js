76f9578af2a463a96482c00e3356d0696300694d
Xinzhe.Gao <gao-yiqian@outlook.com>
2023-11-02 20:15:39 +0900

    新增关键词

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '信哲(Xinzhe)', // 您的昵称
   BIO: process.env.NEXT_PUBLIC_BIO || 'Dream it. Chase it. Code it.', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.shintetu.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '信哲', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '信哲,高信哲,Xinzhe,信哲的博客,信哲のブログ,', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'gao-yiqian@outlook.com', // 邮箱地址
